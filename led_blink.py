5f24be52d61d867371b4a194ee2d64947bff1667
dewalg <dewalgupta@gmail.com>
2015-06-15 18:55:14 -0700

    init commit

---
+++ led_blink.py
@@ -0,0 +1,19 @@
+# python 3 using RPi GPIO library
+import RPi.GPIO as gp
+import time
+
+gp.setmode(gp.BCM)
+
+led = 22
+wait = 0.5 # in seconds
+gp.setup(led, gp.OUT)
+
+try:
+	while 1:
+		gp.output(led, gp.LOW)
+		time.sleep(wait)
+		gp.output(led, gp.HIGH)
+		time.sleep(wait)
+except KeyboardInterrupt:
+	gp.cleanup()
+	
