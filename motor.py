e19bf354204db6f029bc925d142245a4159ad286
dewalg <dewalgupta@gmail.com>
2015-06-17 00:38:15 -0700

    deleted old version of motor.py

--- motor.py
+++ motor.py
@@ -5,22 +5,21 @@ GPIO.setmode(GPIO.BCM)
  
 Motor1A = 23
 Motor1B = 24
-Motor1E = 25
+enable  = 25
  
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
+GPIO.setup(enable,GPIO.OUT)
 try:
 	while 1:
 		print "Turning motor on"
 		GPIO.output(Motor1A,GPIO.HIGH)
 		GPIO.output(Motor1B,GPIO.LOW)
-		GPIO.output(Motor1E,GPIO.HIGH)
+		GPIO.output(enable,GPIO.HIGH)
 		sleep(20)
 except KeyboardInterrupt:
-	GPIO.output(Motor1E, GPIO.LOW)
+	GPIO.output(enable, GPIO.LOW)
 	GPIO.cleanup() 
-#sleep(20)
  
 #print "Stopping motor"
 #GPIO.output(Motor1E,GPIO.LOW)
