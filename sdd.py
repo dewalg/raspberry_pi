34eeaf337a915e3e8af1b0890bb7620fffe0b053
dewal gupta <dewalgupta@gmail.com>
2015-06-22 23:35:18 -0700

    7 digit display code

---
+++ sdd.py
@@ -0,0 +1,66 @@
+# python 2.7.9
+import RPi.GPIO as GPIO
+
+def display_num(num, pin):
+    if num > 9 or num < 0:
+        return 0
+    elif len(pinout) != 7:
+        return 0
+
+    if num == 0:
+        GPIO.output(pin[0])
+        GPIO.output(pin[1])
+        GPIO.output(pin[2])
+        GPIO.output(pin[3])
+        GPIO.output(pin[4])
+        GPIO.output(pin[5])
+    elif num == 1:
+        GPIO.output(pin[1])
+        GPIO.output(pin[2])
+    elif num == 2:
+        GPIO.output(pin[0])
+        GPIO.output(pin[1])
+        GPIO.output(pin[6])
+        GPIO.output(pin[4])
+        GPIO.output(pin[3])
+    elif num == 3:
+        GPIO.output(pin[0])
+        GPIO.output(pin[1])
+        GPIO.output(pin[6])
+        GPIO.output(pin[2])
+        GPIO.output(pin[3])
+    elif num == 4:
+        GPIO.output(pin[5])
+        GPIO.output(pin[6])
+        GPIO.output(pin[1])
+        GPIO.output(pin[2])
+    elif num == 5:
+        GPIO.output(pin[0])
+        GPIO.output(pin[5])
+        GPIO.output(pin[6])
+        GPIO.output(pin[2])
+        GPIO.output(pin[3])
+    elif num == 6:
+        GPIO.output(pin[5])
+        GPIO.output(pin[4])
+        GPIO.output(pin[6])
+        GPIO.output(pin[2])
+        GPIO.output(pin[3])
+    elif num == 7:
+        GPIO.output(pin[0])
+        GPIO.output(pin[1])
+        GPIO.output(pin[2])
+    elif num == 8:
+        GPIO.output(pin[0])
+        GPIO.output(pin[1])
+        GPIO.output(pin[2])
+        GPIO.output(pin[3])
+        GPIO.output(pin[4])
+        GPIO.output(pin[5])
+        GPIO.output(pin[6])
+    elif num == 9:
+        GPIO.output(pin[0])
+        GPIO.output(pin[1])
+        GPIO.output(pin[2])
+        GPIO.output(pin[5])
+        GPIO.output(pin[6])
---
+++ sdd_driver.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from sdd import disp_num
+
+GPIO.setmode(GPIO.BCM)
+
+a = 13
+b = 5
+c = 12
+d = 16
+e = 21
+f = 19
+g = 26
+
+pin = [a,b,c,d,e,f,g]
+
+for p in pin:
+    GPIO.setup(p,GPIO.OUT)
+
+try:
+	for x in range(0,10):
+		print x
+        sdd.disp_num(x,pin)
+		sleep(2)
+    GPIO.cleanup()
+except KeyboardInterrupt:
+	GPIO.cleanup()
