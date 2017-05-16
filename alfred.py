57706c86386cd5bc172b7957051b4f8bbb382a7e
dewalg <dewalg@users.noreply.github.com>
2017-05-15 23:30:20 -0700

    added mic test file

---
+++ alfred.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8-*-
+"""
+    The Mic class handles all interactions with the microphone and speaker.
+"""
+import logging
+import tempfile
+import wave
+import audioop
+import pyaudio
+import requests
+
+class Mic:
+
+    speechRec = None
+    speechRec_persona = None
+
+    def __init__(self, passive_stt_engine):
+        """
+        Initiates the pocketsphinx instance.
+
+        Arguments:
+        speaker -- handles platform-independent audio output
+        passive_stt_engine -- performs STT while Jasper is in passive listen
+                              mode
+        acive_stt_engine -- performs STT while Jasper is in active listen mode
+        """
+        self.logger = logging.getLogger(__name__)
+        self.passive_stt_engine = passive_stt_engine
+        self.logger.info("Initializing PyAudio. ALSA/Jack error messages " +
+                          "that pop up during this process are normal and " +
+                          "can usually be safely ignored.")
+        self._audio = pyaudio.PyAudio()
+        self.logger.info("Initialization of PyAudio completed.")
+
+    def __del__(self):
+        self._audio.terminate()
+
+    def getScore(self, data):
+        rms = audioop.rms(data, 2)
+        score = rms / 3
+        return score
+
+    def passiveListen(self, PERSONA):
+        """
+        Listens for PERSONA in everyday sound. Times out after LISTEN_TIME, so
+        needs to be restarted.
+        """
+
+        THRESHOLD_MULTIPLIER = 1.8
+        RATE = 16000
+        CHUNK = 1024
+
+        # number of seconds to allow to establish threshold
+        THRESHOLD_TIME = 1
+
+        # number of seconds to listen before forcing restart
+        LISTEN_TIME = 10
+
+        # prepare recording stream
+        stream = self._audio.open(format=pyaudio.paInt16,
+                                  channels=1,
+                                  rate=RATE,
+                                  input=True,
+                                  frames_per_buffer=CHUNK)
+
+        # stores the audio data
+        frames = []
+
+        # stores the lastN score values
+        lastN = [i for i in range(30)]
+
+        # calculate the long run average, and thereby the proper threshold
+        for i in range(0, RATE / CHUNK * THRESHOLD_TIME):
+
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+            # save this data point as a score
+            lastN.pop(0)
+            lastN.append(self.getScore(data))
+            average = sum(lastN) / len(lastN)
+
+        # this will be the benchmark to cause a disturbance over!
+        THRESHOLD = average * THRESHOLD_MULTIPLIER
+
+        # save some memory for sound data
+        frames = []
+
+        # flag raised when sound disturbance detected
+        didDetect = False
+
+        # start passively listening for disturbance above threshold
+        for i in range(0, RATE / CHUNK * LISTEN_TIME):
+
+            data = stream.read(CHUNK)
+            frames.append(data)
+            score = self.getScore(data)
+
+            if score > THRESHOLD:
+                didDetect = True
+                break
+
+        # no use continuing if no flag raised
+        if not didDetect:
+            print "No disturbance detected"
+            stream.stop_stream()
+            stream.close()
+            return (None, None)
+
+        # cutoff any recording before this disturbance was detected
+        frames = frames[-20:]
+
+        # otherwise, let's keep recording for few seconds and save the file
+        DELAY_MULTIPLIER = 1
+        for i in range(0, RATE / CHUNK * DELAY_MULTIPLIER):
+
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        # save the audio data
+        stream.stop_stream()
+        stream.close()
+
+        with tempfile.NamedTemporaryFile(mode='w+b') as f:
+            wav_fp = wave.open(f, 'wb')
+            wav_fp.setnchannels(1)
+            wav_fp.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+            wav_fp.setframerate(RATE)
+            wav_fp.writeframes(''.join(frames))
+            wav_fp.close()
+            f.seek(0)
+            # check if PERSONA was said
+            transcribed = self.passive_stt_engine.transcribe(f)
+            self.logger("TRANSCRIBED: " + transcribed)
+
+        if any(PERSONA in phrase for phrase in transcribed):
+            return (THRESHOLD, PERSONA)
+
+        return (False, transcribed)
+
+
+class Wit():
+    """
+    Speech-To-Text implementation which relies on the Wit.ai Speech API.
+    This implementation requires an Wit.ai Access Token to be present in
+    profile.yml. Please sign up at https://wit.ai and copy your instance
+    token, which can be found under Settings in the Wit console to your
+    profile.yml:
+        ...
+        stt_engine: witai
+        witai-stt:
+          access_token:    ERJKGE86SOMERANDOMTOKEN23471AB
+    """
+
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        self.token = "SLTXSOITDXRDNCGD5VKDT6AHWEJA62LZ"
+        self._headers = {'Authorization': 'Bearer %s' % self.token,
+                         'accept': 'application/json',
+                         'Content-Type': 'audio/wav'}
+
+    def transcribe(self, fp):
+        data = fp.read()
+        r = requests.post('https://api.wit.ai/speech?v=20150101',
+                          data=data,
+                          headers=self.headers)
+        try:
+            r.raise_for_status()
+            text = r.json()['_text']
+        except requests.exceptions.HTTPError:
+            self._logger.critical('Request failed with response: %r',
+                                  r.text,
+                                  exc_info=True)
+            return []
+        except requests.exceptions.RequestException:
+            self._logger.critical('Request failed.', exc_info=True)
+            return []
+        except ValueError as e:
+            self._logger.critical('Cannot parse response: %s',
+                                  e.args[0])
+            return []
+        except KeyError:
+            self._logger.critical('Cannot parse response.',
+                                  exc_info=True)
+            return []
+        else:
+            transcribed = []
+            if text:
+                transcribed.append(text.upper())
+            self._logger.info('Transcribed: %r', transcribed)
+            return transcribed
+
+
+if __name__ == "__main__":
+    
+    logging.basicConfig()
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    persona = "ALFRED"
+
+    wit = Wit()
+    mic = Mic(wit)
+    while True:
+        logger.info("Started listening for keyword '%s'",
+                           persona)
+        threshold, transcribed = mic.passiveListen(persona)
+        logger.info("Stopped listening for keyword '%s'",
+                           persona)
+
+        if not transcribed or not threshold:
+            logger.info("Nothing has been said or transcribed.")
+            continue
+        logger.info("Keyword '%s' has been said!", persona)
+
+        logger.info("Started to listen actively with threshold: %r",
+                           threshold)
+        input = mic.activeListenToAllOptions(threshold)
+        logger.info("Stopped to listen actively with threshold: %r",
+                           threshold)
